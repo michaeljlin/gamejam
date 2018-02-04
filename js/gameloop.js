c8664b36f28c0f6a734670e14ea6fa68afe4592d
Tim Horist <TimelikeClosure@users.noreply.github.com>
2018-02-04 12:58:58 -0800

    added core interface for game event loop

--- js/gameloop.js
+++ js/gameloop.js
@@ -1,4 +1,35 @@
 "use strict";
 
+const gameLoop = (function(){
+    /**
+     * Main game event loop function
+     */
+    const main = function(){
 
+    };
+
+    let timeoutId = null;
+
+    main.start = function(){
+
+    };
+
+    main.stop = function(){
+
+    };
+
+    main.pause = function(){
+
+    };
+
+    main.addListener = function(){
+
+    };
+
+    main.removeListener = function(){
+
+    };
+
+    return main;
+})();
 
